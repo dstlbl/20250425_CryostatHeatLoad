0875bdcb3331f625b6b07697dc81674b12b2a032
Damon Todd <dstodd@lbl.gov>
2025-04-23 14:47:03 -0700

    added program outline

---
+++ MeasureHeatLoad.py
@@ -0,0 +1,16 @@
+
+
+###  Magnetic field settings
+
+
+### Power settings
+
+
+### Main loop
+
+#### Set magnet
+#### Set power
+#### Wait some amount of time or watch to make sure settled
+###### Stop if heater power goes to zero
+######   If it goes to zero, reduce power
+##### Write result
